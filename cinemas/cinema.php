e847bb305df0676ef714921c2093be4a85a8d69c
felipe1819 <35507662+felipe1819@users.noreply.github.com>
2018-05-03 15:57:28 -0300

    update_final

--- cinemas/cinema.php
+++ cinemas/cinema.php
@@ -13,44 +13,62 @@ include_once 'crud_cad_funciona.php';
 		<link type="text/css" rel="stylesheet" href ="style.css">
 		
 
-		<link href="https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0-beta.3/lumen/bootstrap.min.css" rel="stylesheet" integrity="sha384-lBO0+E/aIJhpRIYjP6dJ1mNYgo3hhUBPcF74XRfOM27g7WmDuitolvnUENdDG4QI" crossorigin="anonymous">
-		<link href="https://fonts.googleapis.com/css?family=Oleo+Script" rel="stylesheet">
+		<link rel="stylesheet" href="style1.css">
+    
+    
 
-		<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.0/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-kCsv8pSAWtRge/+zcLDeqwoWhTQSUX2esQPYOsocgrg1eMj7T2wrTJP348T3mpBU" crossorigin="anonymous">
 
-		<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
 
-		<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Passion+One" rel="stylesheet">
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.bundle.min.js" integrity="sha384-VspmFJ2uqRrKr3en+IG0cIq1Cl/v/PHneDw6SQZYgrcr8ZZmZoQ3zhuGfMnSR/F2" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-	  
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-	    <span class="navbar-toggler-icon"></span>
-	  </button>
+    
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-	  <div class="collapse navbar-collapse" id="navbarColor01">
-	    <ul class="navbar-nav mr-auto">
-	      
-	      <li class="nav-item">
-	        <a class="nav-link" href=""></a>
-	      </li>
-	      <li class="nav-item">
-	        <a class="nav-link" href="">Cinemas</a>
-	      </li>
-	      <li class="nav-item">
-	        <a class="nav-link" href=""></a>
-	      </li>
-	      <li class="nav-item">
-	        <a class="nav-link" href="../cartaz/cartazfilme.php">Filmes em Sessão</a>
-	      </li>
-	    </ul>
-	    <form class="form-inline my-2 my-lg-0" action="../busca.php" method="post">
-	      <input name="buscar" class="form-control mr-sm-2" type="text" placeholder="Buscar">
-	      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
-	    </form>
-	  </div>
-	</nav>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav mr-auto">
+        
+        <li class="nav-item">
+          <a class="nav-link" href="../cartaz/cartazfilme.php">Filmes em Cartaz</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../cinemas/cinema.php">Cinemas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../cep.php">C.E.P</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../formulario_cinema.php">Cadastro de Cinema</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../formulario_sala2.php">Cadastro de Sala</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../cadasator.php">Cadastro de Ator</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../cadastro_diretor.php">Cadastro de Diretor</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../cadasfilme.php">Cadastro de Filmes</a>
+        </li>
+      </ul>
+      
+    </div>
+  </nav>
 
 	</br>
 
